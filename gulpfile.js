3978993cb1d99a8efc7527836e19c8ff794dc2c8
Vadim Shpet <vadik.shpet01@gmail.com>
2020-08-13 20:50:27 +0300

    half done page Service

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const srcmaps = require('gulp-sourcemaps');
+const less = require('gulp-less');
+
+
+const styleFiles = [
+    './src/css/main.less',
+    './src/css/service.less',
+    './src/css/contacts.less',
+    './src/css/about_us.less',
+    './src/css/client.less'
+]
+const jsFiles = [
+    './src/js/main.js'
+]
+
+gulp.task('styles', () => {
+    //шаблон для поиска файлов стилей
+    return gulp.src(styleFiles)
+    .pipe(srcmaps.init())
+    .pipe(less()) 
+    //объеденение файлов в один
+    .pipe(concat('style.css'))
+
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(cleanCSS({
+        level: 2
+    }))
+
+    .pipe(srcmaps.write('./'))
+    //выходная папка для стилей
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', () => {
+    return gulp.src(jsFiles)
+
+    .pipe(concat('script.js')) 
+
+    .pipe(uglify({
+        toplevel: true
+    }))
+
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream());
+}
+);
+
+
+gulp.task('del', ()=> {
+    return del(['build/*'])
+});
+
+gulp.task('watch', ()=>{
+    browserSync.init({
+        server:{
+            baseDir: "./"
+        }
+    });
+    //следить за изменением файлов
+    gulp.watch('./src/css/**/*.less', gulp.series('styles'))
+    gulp.watch('./src/js/**/*.js', gulp.series('scripts'))
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts'), 'watch'));
